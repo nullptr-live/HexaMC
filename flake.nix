aedfab0ff02d66528a072ce1afd72544d698e923
casually-blue <73141854+casually-blue@users.noreply.github.com>
2022-10-17 15:28:33 -0500

    Point nbtplusplus at our fork for nix packages (#2)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-    libnbtplusplus = { url = "github:PolyMC/libnbtplusplus"; flake = false; };
+    libnbtplusplus = { url = "github:nullptr-live/libnbtplusplus"; flake = false; };
     tomlplusplus = { url = "github:marzer/tomlplusplus"; flake = false; };
   };
 
